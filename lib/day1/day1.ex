210f004cfdf5f58cd32a0a27579d50e39aa68cc2
Meconer <mats.asson@gmail.com>
2025-10-16 09:03:09 +0200

    Day1

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -9,11 +9,47 @@ defmodule Day1 do
 
         # Process the list of lines
         IO.puts("Total lines read: #{length(lines)}")
-
-      # ... further list processing
+        lines
 
       {:error, reason} ->
         IO.puts("Error reading file: #{reason}")
     end
+  end
+
+  def solve_part1(filename) do
+    input = read_input(filename)
+
+    pairs =
+      input
+      |> Enum.map(fn line -> String.split(line) end)
+      |> Enum.map(fn [a, b] -> [String.to_integer(a), String.to_integer(b)] end)
+
+    list1 = Enum.map(pairs, fn [a, _b] -> a end) |> Enum.sort()
+    list2 = Enum.map(pairs, fn [_a, b] -> b end) |> Enum.sort()
+
+    Enum.zip(list1, list2)
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
+  end
+
+  def solve_part2(filename) do
+    input = read_input(filename)
+
+    pairs =
+      input
+      |> Enum.map(fn line -> String.split(line) end)
+      |> Enum.map(fn [a, b] -> [String.to_integer(a), String.to_integer(b)] end)
+
+    list1 = Enum.map(pairs, fn [a, _b] -> a end)
+    list2 = Enum.map(pairs, fn [_a, b] -> b end)
+
+    counts =
+      Enum.map(list1, fn a ->
+        Enum.count(list2, fn b -> b == a end)
+      end)
+
+    Enum.zip(list1, counts)
+    |> Enum.map(fn {a, c} -> a * c end)
+    |> Enum.sum()
   end
 end
