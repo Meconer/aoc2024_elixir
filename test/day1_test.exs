fef9816d4c8e1f85374378753bb4688ce9db47f6
Meconer <mats.asson@gmail.com>
2025-10-16 09:05:05 +0200

    change tests

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,14 +2,11 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  @tag day1: true
-  describe("Day1 tests") do
-    test "Test example part 1" do
-      assert Day1.solve_part1("example.txt") == 11
-    end
+  test "Test example part 1" do
+    assert Day1.solve_part1("example.txt") == 11
+  end
 
-    test "Test example part 2" do
-      assert Day1.solve_part2("example.txt") == 31
-    end
+  test "Test example part 2" do
+    assert Day1.solve_part2("example.txt") == 31
   end
 end
