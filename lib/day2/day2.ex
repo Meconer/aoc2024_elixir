054570497d93f58c0ac1d0494e211c82436a131a
Meconer <mats.asson@gmail.com>
2025-10-17 10:04:33 +0200

    Day2 part 1 complete

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -5,18 +5,25 @@ defmodule Day2 do
     |> Enum.map(fn {a, b} -> b - a end)
   end
 
-  def valid_incr(a, b) do
-    diff = b - a
-    diff >= 1 && diff <= 3
-  end
+  def valid_incr(diff), do: diff >= 1 && diff <= 3
+  def valid_decr(diff), do: diff >= -3 && diff <= -1
 
   def is_valid(line) do
-    _numbers = String.split(line)
-    true
+    diffs =
+      String.split(line)
+      |> Enum.map(&String.to_integer/1)
+      |> makediffs()
+
+    is_valid_incr = Enum.all?(diffs, fn d -> valid_incr(d) end)
+    is_valid_decr = Enum.all?(diffs, fn d -> valid_decr(d) end)
+
+    is_valid_decr || is_valid_incr
   end
 
   def solve_part1(is_example) do
-    lines = ReadInput.read_lines(is_example, 2)
+    lines =
+      ReadInput.read_lines(is_example, 2)
+
     Enum.count(lines, fn line -> is_valid(line) end)
   end
 end
