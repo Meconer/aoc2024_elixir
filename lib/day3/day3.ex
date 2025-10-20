d9f25644ecbf230ce961ec6c7350574487c8d0a1
Meconer <mats.asson@gmail.com>
2025-10-20 15:28:47 +0200

    Day3 completed

--- lib/day3/day3.ex
+++ lib/day3/day3.ex
@@ -1,8 +1,7 @@
 defmodule Day3 do
-  def solve_part1(is_example) do
-    input = ReadInput.read_input(is_example, 3)
+  def do_muls(s) do
     regex = ~r/mul\((\d+),(\d+)\)/
-    matches = Regex.scan(regex, input)
+    matches = Regex.scan(regex, s)
 
     Enum.map(matches, fn [_full_match, t1s, t2s] ->
       t1 = String.to_integer(t1s)
@@ -10,5 +9,42 @@ defmodule Day3 do
       t1 * t2
     end)
     |> Enum.sum()
+  end
+
+  def solve_part1(is_example) do
+    input = ReadInput.read_input(is_example, 3)
+    do_muls(input)
+  end
+
+  def remove_to_next_do(s) do
+    case :binary.match(s, "do()") do
+      :nomatch ->
+        ""
+
+      {index, match_length} ->
+        str_len = String.length(s)
+        rem_len = str_len - index - match_length
+        String.slice(s, index + match_length, rem_len)
+    end
+  end
+
+  def remove_donts(s) do
+    case :binary.match(s, "don't()") do
+      {index, match_length} ->
+        str_len = String.length(s)
+        rem_len = str_len - index - match_length
+        rem_str_before = String.slice(s, 0, index)
+        rem_str_after = remove_to_next_do(String.slice(s, index + match_length, rem_len))
+        rem_str_before <> remove_donts(rem_str_after)
+
+      :nomatch ->
+        s
+    end
+  end
+
+  def solve_part2(is_example) do
+    input = ReadInput.read_input(is_example, 3)
+    input = remove_donts(input)
+    do_muls(input)
   end
 end
