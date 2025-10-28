6907ca4bfc913e75ac0af26310d00cfb21a60f60
Meconer <mats.asson@gmail.com>
2025-10-28 08:35:12 +0100

    Day5 wip

--- lib/day5/day5.ex
+++ lib/day5/day5.ex
@@ -1,12 +1,23 @@
 defmodule Day5 do
   def solve_part1(is_example) do
-    [a,b]  = ReadInput.read_input( is_example, 5)
-    |> String.split("\n\n", trim: true)
-    a = String.split(a, "\n", trim: true)
-    |> Enum.map(&String.split(&1, "|", trim: true))
+    [a, b] =
+      ReadInput.read_input(is_example, 5)
+      |> String.split("\n\n", trim: true)
 
-    b = String.split(b, "\n", trim: true)
-    |> Enum.map(&String.split(&1, ",", trim: true))
-    {a,b}
+    a =
+      String.split(a, "\n", trim: true)
+      |> Enum.map(&String.split(&1, "|", trim: true))
+      |> Enum.map(fn [left, right] ->
+        left_words = String.to_integer(left)
+        right_words = String.to_integer(right)
+        {left_words, right_words}
+      end)
+
+    b =
+      String.split(b, "\n", trim: true)
+      |> Enum.map(&String.split(&1, ",", trim: true))
+      |> Enum.map(fn list -> Enum.map(list, &String.to_integer/1) end)
+
+    {a, b}
   end
 end
