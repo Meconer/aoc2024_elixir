68b2e2b6871e2f272501a653db41ff7587628f25
Meconer <mats.asson@gmail.com>
2025-10-31 16:06:02 +0100

    Day6 wip

--- lib/day6/day6.ex
+++ lib/day6/day6.ex
@@ -1,7 +1,22 @@
 defmodule Day6 do
+  def find_in_grid(grid, ch_to_find) do
+    grid_as_lists =
+      Tuple.to_list(grid)
+      |> Enum.map(&Tuple.to_list/1)
+
+    Enum.find_index(grid_as_lists, fn row ->
+      col =
+        Enum.find_index(row, fn c ->
+          c == ch_to_find
+        end)
+
+      col
+    end)
+  end
+
   def solve_part1(is_example) do
     {grid, width, height} = ReadInput.read_grid(is_example, 6)
-    grid
+    {r, c} = find_in_grid(grid, ?^)
     # GridFunc.get_char(1, 2, grid)
   end
 end
