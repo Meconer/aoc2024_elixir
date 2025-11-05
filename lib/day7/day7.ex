cdf895cee2862c05402940ea4cd042b155548ec1
Meconer <mats.asson@gmail.com>
2025-11-05 19:10:08 +0100

    Day 7 complete

--- lib/day7/day7.ex
+++ lib/day7/day7.ex
@@ -34,14 +34,56 @@ defmodule Day7 do
     end
   end
 
-  def calc_possible_results({wanted, terms}) do
-    IO.puts("W: #{wanted}")
+  defmodule CalculatorP2 do
+    defp possible_results([a, b]) do
+      concat =
+        (Integer.to_string(b) <> Integer.to_string(a))
+        |> String.to_integer()
+
+      [a + b, a * b, concat]
+    end
+
+    defp possible_results([a | rest]) do
+      prev_results = possible_results(rest)
+
+      Enum.flat_map(prev_results, fn el ->
+        concat =
+          (Integer.to_string(el) <> Integer.to_string(a))
+          |> String.to_integer()
+
+        [a + el, a * el, concat]
+      end)
+    end
+
+    def calc_all(terms) do
+      rev_terms = Enum.reverse(terms)
+      possible_results(rev_terms)
+    end
+  end
+
+  def is_valid({wanted, terms}) do
     Calculator.calc_all(terms)
+    |> Enum.any?(fn res_el -> res_el == wanted end)
   end
 
   def solve_part1(is_example) do
     input = parse_input(is_example)
 
-    Enum.map(input, fn line -> calc_possible_results(line) end)
+    Enum.reduce(input, 0, fn {wanted, _} = line, acc ->
+      if is_valid(line), do: acc + wanted, else: acc
+    end)
+  end
+
+  def is_validP2({wanted, terms}) do
+    CalculatorP2.calc_all(terms)
+    |> Enum.any?(fn res_el -> res_el == wanted end)
+  end
+
+  def solve_part2(is_example) do
+    input = parse_input(is_example)
+
+    Enum.reduce(input, 0, fn {wanted, _} = line, acc ->
+      if is_validP2(line), do: acc + wanted, else: acc
+    end)
   end
 end
