7df3d6920d6a9c9baa430542ff0c726013ddda7a
Meconer <mats.asson@gmail.com>
2025-11-07 10:09:41 +0100

    Day8 wip

--- lib/day8/day8.ex
+++ lib/day8/day8.ex
@@ -5,21 +5,47 @@ defmodule Day8 do
     |> Enum.map(fn {line, row_idx} ->
       {Enum.with_index(String.to_charlist(line)), row_idx}
     end)
-    |> Enum.reduce([], fn {day_list, row_idx}, acc ->
-      found =
-        Enum.reduce(day_list, [], fn {ch, col_idx}, acc ->
-          if ch != 46 do
-            [{{row_idx, col_idx}, ch} | acc]
-          else
-            acc
-          end
-        end)
-
-      if length(found) > 0 do
-        [found | acc]
-      else
-        acc
-      end
+    |> Enum.reduce(Map.new(), fn {day_list, row_idx}, acc ->
+      Enum.reduce(day_list, acc, fn {ch, col_idx}, acc ->
+        if ch != 46 do
+          pos_list = Map.get(acc, ch, [])
+          Map.put(acc, ch, [{row_idx, col_idx} | pos_list])
+        else
+          acc
+        end
+      end)
     end)
+  end
+
+  def calc_anti_nodes({r1, c1}, {r2, c2}) do
+    dx = c2 - c1
+    dy = r2 - r1
+    ar1 = r1 + 2 * dy
+    ac1 = c1 + 2 * dx
+    ar2 = r2 - 2 * dy
+    ac2 = c2 - 2 * dx
+    [{ar1, ac1}, {ar2, ac2}]
+  end
+
+  def solve_part1(is_example) do
+    input = get_input(is_example)
+
+    all_anti_nodes =
+      Enum.reduce(input, [], fn {ch, pos_list}, all_an_acc ->
+        pairs = Comparer.compare_all_unique_pairs(pos_list)
+        IO.puts("#{ch} - ")
+
+        anti_nodes_for_name =
+          Enum.reduce(pairs, [], fn {{r1, c1}, {r2, c2}}, an_acc ->
+            antinodes = calc_anti_nodes({r1, c1}, {r2, c2})
+
+            an_acc ++ antinodes
+          end)
+
+        IO.inspect(anti_nodes_for_name, label: "antinodes for #{ch}")
+        all_an_acc ++ anti_nodes_for_name
+      end)
+
+    all_anti_nodes
   end
 end
